2ee6a4808f388fee9a50295459a15fa2887f0216
SalmonLoki <kseniiakrovlya@gmail.com>
2018-11-16 18:09:58 +0300

    remove garbage

--- words_count/main.cpp
+++ words_count/main.cpp
@@ -46,55 +46,46 @@ int amount_with_m128i(char *str, size_t size) {
     }
     if (i != 0)
         check1 = true;
-    //cout << "check1 = " << ((check1) ? "true" : "false") << "\n";
 
     count += check(str, i);
 
-    if (i + 1 + 16 < size) { //проверка, будет ли check16
+    if (i + 1 + 16 < size) {
         check16 = true;
-        //cout << "check16 = " << ((check16) ? "true" : "false") << "\n";
         check16Bound = str + i;
 
         int k = 0;
-        if (*(str + i) != ' ') //если строка для check16 начинается не с пробела, это не будет посчитано как граница слов
+        if (*(str + i) != ' ')
             k++;
 
         for (;  i + 1 + 16 < size; i += 16) {
-            //cout << "new 16" << "\n";
 
             __m128i curString = _mm_cmpgt_epi8(_mm_load_si128((__m128i *) (str + i)), SPACE);
 
             __m128i stringRightShift = _mm_cmpgt_epi8(_mm_loadu_si128((__m128i *) (str + i + 1)),
-                                                      SPACE);//результат будет 1111 1111 если нет пробелов (еслие сть пробелы, будут нули)
+                                                      SPACE);
 
-
-            k += __builtin_popcount( _mm_movemask_epi8(_mm_xor_si128(stringRightShift, curString))); //кол-во 1 битов в k
-            //cout << "i = " << i << "\n";
-
+            k += __builtin_popcount( _mm_movemask_epi8(_mm_xor_si128(stringRightShift, curString)));
         }
-        //cout << "i = " << i << "\n";
 
-        if (  *(str + i ) != ' ' ) //если строка для check16 заканчивается  не пробелом, то это не будет посчитано как граница
+        if (  *(str + i ) != ' ' )
             k++;
 
-        //cout << "k = " << k << "\n";
         count += (k / 2);
     }
 
     if (check16) {
-        if ((i + 1) < size) { //проверка будет ли чек2
+        if ((i + 1) < size) {
             check2 = true;
             check2Bound = str + i + 1;
             count += check(str + i + 1, size - (i + 1));
         }
     } else {
-        if (i < size) { //проверка будет ли чек2
+        if (i < size) {
             check2 = true;
             check2Bound = str + i;
             count += check(str + i, size - i);
         }
     }
-
 
     //проверка на стыках
     if (check1 && check16 && *(check16Bound - 1) != ' ' && *(check16Bound) != ' ')
@@ -120,7 +111,6 @@ int main() {
         srand(time(NULL) + c);
         for( int i = 0; i < size; i++)
             a[i] = rand() % 2 == 0 ? ' ' : 'a';
-
 
 
         for (int j = 0; j < size; j++) {
@@ -155,8 +145,7 @@ int main() {
             break;
         }
 
-
-
+        
 
     (testOk) ?  printf("yes\n") :  printf("no\n");
 
